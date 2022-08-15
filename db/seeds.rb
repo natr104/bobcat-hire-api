eba9d25d7f5f98164399e1d87c936fd21c68d833
natr104 <troy.nathan@gmail.com>
2022-08-15 14:54:37 +1000

    create job category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = Category.create([{ name: 'Pool' }, { name: 'Animal Burial' }, { name: 'Land Levelling' }, { name: 'Driveway' }, { name: 'Dirt/Rubble Cleanup' }, { name: 'Trench Backfill' }, { name: 'Motorcross Track' }, { name: 'Other - Please Specify' }])
