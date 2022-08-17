9f261e2919356da01936dfd089941ea1fda69e56
natr104 <troy.nathan@gmail.com>
2022-08-17 22:57:26 +1000

    add admin to user serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :address, :phone_no
+  attributes :id, :name, :email, :address, :phone_no, :admin
   has_many :jobs
 end
