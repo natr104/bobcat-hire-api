94cbc329bf7ff64c9d3d9d26c1ec95a81221703b
natr104 <troy.nathan@gmail.com>
2022-08-28 12:56:36 +1000

    add user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 categories = Category.create([{ name: 'Pool' }, { name: 'Animal Burial' }, { name: 'Land Levelling' }, { name: 'Driveway' }, { name: 'Dirt/Rubble Cleanup' }, { name: 'Trench Backfill' }, { name: 'Motorcross Track' }, { name: 'Other - Please Specify' }])
+users = User.create(email: 'troy.nathan@gmail.com', name: 'Nathan Troy', password: 'password', password_confirmation: 'password', phone_no: '', address: '123 Fake Street')
